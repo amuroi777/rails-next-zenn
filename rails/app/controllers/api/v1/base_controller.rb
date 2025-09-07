418f6d4bdaf6d8eef969f108f7484c325d9c88a3
muroi-a <akihito.muroi@x-naut.jp>
2025-09-07 15:24:39 +0900

    Api::V1::BaseControllerを実装

---
+++ rails/app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::HealthCheckController < ApplicationController
+  alias_method :current_user, :current_api_vi_user
+  alias_method :authenticate_user!, :authenticate__api_vi_user!
+  alias_method :user_signed_in?, :api_vi_user_signed_in?
+end

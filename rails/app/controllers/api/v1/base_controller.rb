3ba30b2242c53a5f5ce452877f0a2303c791ed28
muroi-a <akihito.muroi@x-naut.jp>
2025-09-07 19:59:34 +0900

    frozen error修正

--- rails/app/controllers/api/v1/base_controller.rb
+++ rails/app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::HealthCheckController < ApplicationController
+class Api::V1::BaseController < ApplicationController
   alias_method :current_user, :current_api_vi_user
   alias_method :authenticate_user!, :authenticate__api_vi_user!
   alias_method :user_signed_in?, :api_vi_user_signed_in?

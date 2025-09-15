60dd2a2230a9eed3ca9a18c73af03a601c68c231
muroi-a <akihito.muroi@x-naut.jp>
2025-09-15 10:46:41 +0900

    修正漏れ対応

--- rails/app/controllers/api/v1/base_controller.rb
+++ rails/app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BaseController < ApplicationController
-  alias_method :current_user, :current_api_vi_user
-  alias_method :authenticate_user!, :authenticate__api_vi_user!
-  alias_method :user_signed_in?, :api_vi_user_signed_in?
+  # DeviseTokenAuthのメソッドを直接使用
+  # current_userは既にDeviseTokenAuthで定義されている
+  # 認証が必要な場合はbefore_actionでauthenticate_user!を使用
 end

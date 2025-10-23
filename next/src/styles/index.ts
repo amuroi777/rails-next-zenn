371a9d199ca7c0a90ca58f5a2a86862afec262d7
amuroi777 <muroiaki7722@gmail.com>
2025-10-23 22:02:27 +0900

    Lintエラー修正

--- next/src/styles/index.ts
+++ next/src/styles/index.ts
@@ -1,7 +1,7 @@
-import { css } from "@mui/system";
+import { css } from '@mui/system'
 
 export const styles = {
   pageMinHeight: css({
-    minHeight: "calc(100vh - 64px)",
+    minHeight: 'calc(100vh - 64px)',
   }),
-};
+}

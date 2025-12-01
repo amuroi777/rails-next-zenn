72fb02fad7aca4c41c1370626d1e57172f09cf65
muroi <amuro683@gmail.com>
2025-12-01 09:51:18 +0900

    entrypointコマンド修正

--- rails/entrypoint.prod.sh
+++ rails/entrypoint.prod.sh
@@ -3,17 +3,21 @@ set -e
 
 echo "Start entrypoint.prod.sh"
 
+echo "mkdir -p /myapp/tmp/sockets"
+mkdir -p /myapp/tmp/sockets
+
 echo "rm -f /myapp/tmp/pids/server.pid"
 rm -f /myapp/tmp/pids/server.pid
 
+# ★ 修正点: データベースコマンドを追加 ★
 echo "bundle exec rails db:create RAILS_ENV=production"
 bundle exec rails db:create RAILS_ENV=production
 
 echo "bundle exec rails db:migrate RAILS_ENV=production"
 bundle exec rails db:migrate RAILS_ENV=production
 
-echo "bundle exec rails db:seed RAILS_ENV=production"
-bundle exec rails db:seed RAILS_ENV=production
+# echo "bundle exec rails db:seed RAILS_ENV=production"
+# bundle exec rails db:seed RAILS_ENV=production
 
-echo "exec pumactl start"
-bundle exec pumactl start
+echo "exec bundle exec puma -C config/puma.rb"
+exec bundle exec puma -C config/puma.rb
